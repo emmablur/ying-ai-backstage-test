0ca7905e36035100425ef947bb78517a6ac8daa0
emmablur <emmablur@users.noreply.github.com>
2025-06-14 05:26:01 +0000

    deploy: emmablur/ying-ai-backstage@42f96d8c333f217273ea1d94508b6795a4b36c32

---
+++ browser/chunk-RLDRT3DX.js
@@ -0,0 +1,1 @@
+var t={production:!0,siteTitle:"\u5F8C\u53F0\u6B63\u5F0F\u7AD9-\u82F1\u611B\u624B\u4F5C",basePath:"https://api.yingaihandmade.com",apiUrl:"https://api.yingaihandmade.com"};export{t as a};
